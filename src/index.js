55c0389b46462baa56a1bbe5e4b2907cfe5ad51e
mlgvla <mlgerardvla@gmail.com>
2022-01-13 22:45:37 -0500

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  installEventListeners()
 });
+
+function installEventListeners() {
+    let form = document.getElementById("create-task-form")
+    
+    form.addEventListener("submit", addTask)
+}
+
+function addTask(e) {
+    e.preventDefault()
+    let toDoInput = document.getElementById("new-task-description")
+    let ul = document.getElementById("tasks")
+    let li = document.createElement("li")
+    let deleteBtn = document.createElement("button")
+    deleteBtn.innerHTML = "x"
+
+    deleteBtn.addEventListener("click", (e) => {
+        e.target.parentNode.remove()
+    })
+    
+
+    li.innerHTML = `${toDoInput.value}  `
+    li.appendChild(deleteBtn)
+    ul.appendChild(li)
+
+    toDoInput.value = ""
+    
+}
